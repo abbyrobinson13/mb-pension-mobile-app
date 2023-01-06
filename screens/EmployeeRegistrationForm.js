d876128a0e2b2eee405fbd568f561da77fb45cd0
Mariela <mvmihilova@gmail.com>
2023-01-05 20:38:21 -0700

    form: to be updated next

---
+++ screens/EmployeeRegistrationForm.js
@@ -0,0 +1,21 @@
+import {View, Text, Button} from 'react-native';
+import AppStyles from '../styles/AppStyles';
+
+export default function EmployeeRegistrationForm({navigation}) {
+  const background = require ('../assets/background.jpg');
+
+  return (
+    <ImageBackground style={AppStyles.container} source={background}>
+      <KeyboardAvoidingView
+        style={AppStyles.backgroundCover}
+        behavior={Platform.OS === 'ios' ? padding : null}
+        keyboardVerticalOffset={60}
+      >
+        <Text style={AppStyles.lightText}>
+          Afshin, you need to add the REGISTRATION FORM HERE
+        </Text>
+        <Button title="Submit" onPress={signUp} color={'#01796f'} />
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+}
