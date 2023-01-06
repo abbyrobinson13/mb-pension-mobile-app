dc300528627ad9f523c9b075850b296be15de1ec
Mariela <mvmihilova@gmail.com>
2023-01-05 20:34:23 -0700

    Navigation

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './screens/Login';
+import SignUp from './screens/SignUp';
+import ResetPassword from './screens/ResetPassword';
+import EmployeeRegistrationForm from './screens/EmployeeRegistrationForm';
 
-export default function App() {
+const Stack = createNativeStackNavigator ();
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text> Project 3 Mobile App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Sign Up"
+          component={SignUp}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Reset Password"
+          component={ResetPassword}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Employee Registration Form"
+          component={EmployeeRegistrationForm}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
