b13242b29d6e025f31b25d7f18a6d65e8cd165b6
Mariela <mvmihilova@gmail.com>
2023-01-05 20:40:00 -0700

    just a placeholder

---
+++ screens/PlaceholderScreen.js
@@ -0,0 +1,12 @@
+import {View, Text, Button} from 'react-native';
+import AppStyles from '../styles/AppStyles';
+
+export default function TBDForm({navigation}) {
+  return (
+    <View style={AppStyles.container}>
+      <Button title="Placeholder" />
+    </View>
+  );
+}
+
+//TODO: Need to determine the content of this screen
