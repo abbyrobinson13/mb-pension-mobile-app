a1534afb8a51f6c64fc62df363093c5af9435425
Mariela <mvmihilova@gmail.com>
2023-01-06 13:39:27 -0700

    fixed background issue

--- screens/PlaceholderScreen.js
+++ screens/PlaceholderScreen.js
@@ -1,11 +1,27 @@
-import {View, Text, Button} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  KeyboardAvoidingView,
+} from 'react-native';
 import AppStyles from '../styles/AppStyles';
+import background from '../assets/background.jpg';
 
 export default function PlaceholderScreen({navigation}) {
   return (
-    <View style={AppStyles.container}>
-      <Button title="Placeholder" />
-    </View>
+    <ImageBackground style={AppStyles.container} source={background}>
+      <KeyboardAvoidingView
+        style={AppStyles.backgroundCover}
+        behavior={Platform.OS === 'ios' ? padding : null}
+        keyboardVerticalOffset={60}
+      >
+        <Text style={AppStyles.lightText}>
+          PLACEHOLDER SCREEN!
+          WHO IS TAKING IT FROM HERE?
+        </Text>
+      </KeyboardAvoidingView>
+    </ImageBackground>
   );
 }
 
