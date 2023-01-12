d80bd54c00c0ee8fee6c112f3680eef90e8c9d04
Abigail Robinson <abbyrobinson@shaw.ca>
2023-01-12 09:26:54 -0700

    edit ios padding

--- screens/EmployeeRegistrationForm.js
+++ screens/EmployeeRegistrationForm.js
@@ -4,23 +4,23 @@ import {
   Button,
   ImageBackground,
   KeyboardAvoidingView,
-} from 'react-native';
-import AppStyles from '../styles/AppStyles';
+} from "react-native";
+import AppStyles from "../styles/AppStyles";
 
-export default function EmployeeRegistrationForm({navigation}) {
-  const background = require ('../assets/background.jpg');
+export default function EmployeeRegistrationForm({ navigation }) {
+  const background = require("../assets/background.jpg");
 
   return (
     <ImageBackground style={AppStyles.container} source={background}>
       <KeyboardAvoidingView
         style={AppStyles.backgroundCover}
-        behavior={Platform.OS === 'ios' ? padding : null}
+        behavior={Platform.OS === "ios" ? "padding" : null}
         keyboardVerticalOffset={60}
       >
         <Text style={AppStyles.lightText}>
           Afshin, you need to add the REGISTRATION FORM HERE
         </Text>
-        <Button title="Submit" color={'#01796f'} />
+        <Button title="Submit" color={"#01796f"} />
       </KeyboardAvoidingView>
     </ImageBackground>
   );
