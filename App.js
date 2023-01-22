0ea3792553e6634b0ea86c88ea6cab2426fd1dae
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-01-22 15:56:13 -0700

    modified App.js- navigate to questionnare  after registration

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
 import CreatePassword from './screens/CreatePassword';
 import ResetPassword from './screens/ResetPassword';
@@ -9,47 +9,47 @@ import EmployeeRegistrationForm from './screens/EmployeeRegistrationForm';
 import TermsConditionsForm from './screens/TermsConditions';
 import QuestionnaireOne from './screens/QuestionnaireOne';
 
-const Stack = createNativeStackNavigator ();
+const Stack = createNativeStackNavigator();
 
-export default function App () {
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name="Login"
           component={Login}
-          options={{headerShown: true}}
+          options={{ headerShown: true }}
         />
         <Stack.Screen
           name="Create Password"
           component={CreatePassword}
-          options={{headerShown: true}}
+          options={{ headerShown: true }}
         />
         <Stack.Screen
           name="Terms and Conditions"
           component={TermsConditionsForm}
-          options={{headerShown: true}}
+          options={{ headerShown: true }}
         />
         <Stack.Screen
           name="Reset Password"
           component={ResetPassword}
-          options={{headerShown: true}}
+          options={{ headerShown: true }}
         />
 
         <Stack.Screen
           name="Employee Registration Form"
           component={EmployeeRegistrationForm}
-          options={{headerShown: true}}
+          options={{ headerShown: true }}
         />
 
         <Stack.Screen
-          name="Placeholder Screen"
-          component={PlaceholderScreen}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
           name="Questionnaire One"
           component={QuestionnaireOne}
+          options={{ headerShown: true }}
+        />
+        <Stack.Screen
+          name="Placeholder Screen"
+          component={PlaceholderScreen}
           options={{ headerShown: true }}
         />
       </Stack.Navigator>
