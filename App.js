35371a0acdfb784c7b4d6eed9a85afa4df2d49a2
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-01-24 01:04:11 -0700

    feat: imported providers

--- App.js
+++ App.js
@@ -1,58 +1,63 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Login from './screens/Login';
-import CreatePassword from './screens/CreatePassword';
-import ResetPassword from './screens/ResetPassword';
-import PlaceholderScreen from './screens/PlaceholderScreen';
-import EmployeeRegistrationForm from './screens/EmployeeRegistrationForm';
-import TermsConditionsForm from './screens/TermsConditions';
-import QuestionnaireOne from './screens/QuestionnaireOne';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./screens/Login";
+import CreatePassword from "./screens/CreatePassword";
+import ResetPassword from "./screens/ResetPassword";
+import PlaceholderScreen from "./screens/PlaceholderScreen";
+import EmployeeRegistrationForm from "./screens/EmployeeRegistrationForm";
+import TermsConditionsForm from "./screens/TermsConditions";
+import QuestionnaireOne from "./screens/QuestionnaireOne";
+import { FireBaseProvider } from "./firebase";
+import { AuthProvider } from "./AuthProvider";
 
-const Stack = createNativeStackNavigator ();
+const Stack = createNativeStackNavigator();
 
-export default function App () {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name="Create Password"
-          component={CreatePassword}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name="Terms and Conditions"
-          component={TermsConditionsForm}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name="Reset Password"
-          component={ResetPassword}
-          options={{headerShown: true}}
-        />
+    <FireBaseProvider>
+      <AuthProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name="Create Password"
+              component={CreatePassword}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name="Terms and Conditions"
+              component={TermsConditionsForm}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name="Reset Password"
+              component={ResetPassword}
+              options={{ headerShown: true }}
+            />
 
-        <Stack.Screen
-          name="Employee Registration Form"
-          component={EmployeeRegistrationForm}
-          options={{headerShown: true}}
-        />
-
-        <Stack.Screen
-          name="Placeholder Screen"
-          component={PlaceholderScreen}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name="Questionnaire One"
-          component={QuestionnaireOne}
-          options={{ headerShown: true }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+            <Stack.Screen
+              name="Employee Registration Form"
+              component={EmployeeRegistrationForm}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name="Placeholder Screen"
+              component={PlaceholderScreen}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name="Questionnaire One"
+              component={QuestionnaireOne}
+              options={{ headerShown: true }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthProvider>
+    </FireBaseProvider>
   );
 }
