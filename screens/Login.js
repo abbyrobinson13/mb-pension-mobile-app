71f930df0d8843e10618c1a396ef5e3313cb8762
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-01-25 14:46:42 -0700

    Added Logout button

--- screens/Login.js
+++ screens/Login.js
@@ -6,47 +6,47 @@ import {
   KeyboardAvoidingView,
   Platform,
   TouchableOpacity,
-} from 'react-native';
-import AppStyles from '../styles/AppStyles';
-import {Button} from 'react-native-paper';
-import React from 'react';
-import InlineTextButton from '../components/InlineTextButton';
-import {auth} from '../firebase';
-import {signInWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth';
+} from "react-native";
+import AppStyles from "../styles/AppStyles";
+import { Button } from "react-native-paper";
+import React from "react";
+import InlineTextButton from "../components/InlineTextButton";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 
-export default function Login({navigation}) {
-  const background = require ('../assets/background.jpg');
+export default function Login({ navigation }) {
+  const background = require("../assets/background.jpg");
 
   if (auth.currentUser) {
-    navigation.navigate ('Employee Registration Form');
+    navigation.navigate("Employee Registration Form");
   } else {
-    onAuthStateChanged (auth, user => {
+    onAuthStateChanged(auth, (user) => {
       if (user) {
-        navigation.navigate ('Placeholder Screen');
+        navigation.navigate("Logging Out");
       }
     });
   }
 
-  let [errorMessage, setErrorMessage] = React.useState ('');
-  let [email, setEmail] = React.useState ('');
-  let [password, setPassword] = React.useState ('');
+  let [errorMessage, setErrorMessage] = React.useState("");
+  let [email, setEmail] = React.useState("");
+  let [password, setPassword] = React.useState("");
 
   let login = () => {
-    if (email !== '' && password !== '') {
-      signInWithEmailAndPassword (auth, email, password)
-        .then (userCredential => {
-          navigation.navigate ('Placeholder Screen', {
+    if (email !== "" && password !== "") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          navigation.navigate("Logging Out", {
             user: userCredential.user,
           });
-          setErrorMessage ('');
-          setEmail ('');
-          setPassword ('');
+          setErrorMessage("");
+          setEmail("");
+          setPassword("");
         })
-        .catch (error => {
-          setErrorMessage ('User not found. Please, Create a Password');
+        .catch((error) => {
+          setErrorMessage("User not found. Please, Create a Password");
         });
     } else {
-      setErrorMessage ('Please, enter an email and password');
+      setErrorMessage("Please, enter an email and password");
     }
   };
 
@@ -54,7 +54,7 @@ export default function Login({navigation}) {
     <ImageBackground style={AppStyles.container} source={background}>
       <KeyboardAvoidingView
         style={AppStyles.backgroundCover}
-        behavior={Platform.OS === 'ios' ? padding : null}
+        behavior={Platform.OS === "ios" ? padding : null}
         keyboardVerticalOffset={60}
       >
         <Text style={AppStyles.darkText}>Log in</Text>
@@ -75,28 +75,24 @@ export default function Login({navigation}) {
           onChangeText={setPassword}
         />
         <View style={AppStyles.rowContainer}>
-          <Text style={AppStyles.darkText}>
-            Don't have a password, yet?
-          </Text>
+          <Text style={AppStyles.darkText}>Don't have a password, yet?</Text>
           <InlineTextButton
             text=" Create password"
-            onPress={() => navigation.navigate ('Create Password')}
+            onPress={() => navigation.navigate("Create Password")}
           />
         </View>
         <View style={AppStyles.rowContainer}>
-          <Text style={AppStyles.darkText}>
-            Forgotten your password?
-          </Text>
+          <Text style={AppStyles.darkText}>Forgotten your password?</Text>
           <InlineTextButton
             text=" Reset"
-            onPress={() => navigation.navigate ('Reset Password')}
+            onPress={() => navigation.navigate("Reset Password")}
           />
         </View>
         <View>
           <TouchableOpacity>
             <Button
               style={AppStyles.button}
-              labelStyle={{color: 'black', fontSize: 16, fontWeight: 'bold'}}
+              labelStyle={{ color: "black", fontSize: 16, fontWeight: "bold" }}
               onPress={login}
             >
               Log in
