bfa9f479aca10d50082465d0b98f1bba110a049f
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-01-27 13:08:59 -0700

    feat: data for Q2

---
+++ data/areasOfConcern.js
@@ -0,0 +1,16 @@
+import Concern from '../models/Concern.js';
+
+
+export const CONCERNS = [
+  new Concern('c1', 'Anxiety'),
+  new Concern('c2', 'Trauma'),
+  new Concern('c3', 'Depression'),
+  new Concern('c4', 'ADD/ADHD'),
+  new Concern('c5', 'Bipolar Disorder'),
+  new Concern('c6', 'Executive Function'),
+  new Concern('c7', 'Eating Disorders'),
+  new Concern('c8', 'Addiction'),
+  new Concern('c9', 'Schizophrenia'),
+  new Concern('c10', 'Autism')
+];
+
