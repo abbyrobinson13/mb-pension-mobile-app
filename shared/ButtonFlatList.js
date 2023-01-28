cf05aac4fc6dca5d17c4940b76989cbc2baa0731
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-01-27 18:11:57 -0700

    feat: shared component (button)

---
+++ shared/ButtonFlatList.js
@@ -0,0 +1,43 @@
+import { Button, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const ButtonFlatList = (selected) => {
+  const title = "Click Next";
+  return (
+    <TouchableOpacity style={styles.button}>
+    
+      <View>
+      <Text style={styles.text}>{title} </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+export default ButtonFlatList;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    height: 60,
+    width: 300,
+    borderRadius: 40,
+    marginLeft: 50,
+    marginTop: 30,
+    marginHorizontal: 60,
+    elevation: 4,
+    shadowColor: "white",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 1,
+    
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    color: "black",
+    textAlign: "center",
+    marginTop: 15,
+    
+    
+  },
+});
