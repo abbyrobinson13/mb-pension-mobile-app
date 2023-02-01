3dbc0fa0b63b6f3344bf81c8961fcd1b055047e0
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-01 10:04:37 -0700

    chore: changed areas of concerns for Q2

--- data/areasOfConcern.js
+++ data/areasOfConcern.js
@@ -2,15 +2,15 @@ import Concern from '../models/Concern.js';
 
 
 export const CONCERNS = [
-  new Concern('c1', 'Anxiety'),
-  new Concern('c2', 'Trauma'),
-  new Concern('c3', 'Depression'),
-  new Concern('c4', 'ADD/ADHD'),
-  new Concern('c5', 'Bipolar Disorder'),
-  new Concern('c6', 'Executive Function'),
-  new Concern('c7', 'Eating Disorders'),
-  new Concern('c8', 'Addiction'),
-  new Concern('c9', 'Schizophrenia'),
-  new Concern('c10', 'Autism')
+  new Concern('c1', 'ADHD'),
+  new Concern('c2', 'Addiction'),
+  new Concern('c3', 'Anxiety'),
+  new Concern('c4', 'Burn Out & Brain Health'),
+  new Concern('c5', 'Depression'),
+  new Concern('c6', 'Fertility'),
+  new Concern('c7', 'Hormone Health'),
+  new Concern('c8', 'Menopause & Peri-Menopause'),
+  new Concern('c9', 'Diet & Gut Health'),
+  new Concern('c10', 'Trauma')
 ];
 
