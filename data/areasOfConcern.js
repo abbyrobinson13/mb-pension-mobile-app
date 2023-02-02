215b7f6a220828d1f8089e4a531ebb60bb7362c5
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-02 13:31:27 -0700

    chore: added new fields to class

--- data/areasOfConcern.js
+++ data/areasOfConcern.js
@@ -5,12 +5,11 @@ export const CONCERNS = [
   new Concern('c1', 'ADHD'),
   new Concern('c2', 'Addiction'),
   new Concern('c3', 'Anxiety'),
-  new Concern('c4', 'Burn Out & Brain Health'),
+  new Concern('c4', 'Insomnia'),
   new Concern('c5', 'Depression'),
-  new Concern('c6', 'Fertility'),
-  new Concern('c7', 'Hormone Health'),
-  new Concern('c8', 'Menopause & Peri-Menopause'),
-  new Concern('c9', 'Diet & Gut Health'),
-  new Concern('c10', 'Trauma')
+  new Concern('c6', 'Trauma'),
+  new Concern('c7', 'Eating Disorders'),
+  new Concern('c8', 'Emotional Regulation Problem'),
+  
 ];
 
