a0435d96cedda9a4eedbd470ddf48f3708226e97
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-02 13:28:42 -0700

    feat : press handler function for questionnaire two

--- screens/QuestionnaireTwo.js
+++ screens/QuestionnaireTwo.js
@@ -1,32 +1,42 @@
 import { CONCERNS } from "../data/areasOfConcern.js";
-import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import ConcernGridTile from "../components/ConcernGridTile.js";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../AuthProvider.js";
 import { FirebaseContext } from "../firebase.js";
 import ButtonFlatList from "../shared/ButtonFlatList.js";
-
+import { ipAndPort } from "../config.js";
 const QuestionnaireTwo = ({ navigation }) => {
   const fbContext = useContext(FirebaseContext);
   const app = fbContext.app;
   const auth = fbContext.auth;
   const authContext = useContext(AuthContext);
   const user = authContext.user;
-const buttonText ="Click next"
+  const buttonText = "Click next";
   const [employees, setEmployees] = useState(null);
-  const ipAndPort = "10.44.22.29:5001";
+  //const ipAndPort = "10.44.22.75:5001";
   console.log(ipAndPort);
   console.log(auth);
   console.log("user", user);
+  const [picked, setPicked] = useState();
+  console.log(picked);
 
-  const renderConcernItem =(itemData)=> {
+  const renderConcernItem = (itemData, picked, setPicked) => {
     return (
       <ConcernGridTile
         title={itemData.item.title}
-        
+        picked={picked}
+        setPicked={setPicked}
       />
     );
-  }
+  };
   useEffect(() => {
     const getEmployees = async () => {
       try {
@@ -41,53 +51,53 @@ const buttonText ="Click next"
     getEmployees();
   }, []);
 
-  const pressHandler=() => {
+  const pressHandler = async () => {
+    //console.log ('concerns){"Addiction": true, "Anxiety": true}
+    const selectedConcerns = Object.keys(picked).filter((key) => picked[key]);
+    console.log(selectedConcerns);
+    await saveConcerns(selectedConcerns);
+  };
 
-      
-  }
-
-  // // const onPressHandle = async (concern) => {
-  // //   const response = await fetch(
-  // //     `http://${ipAndPort}/api/employee/byEmail/${user.email}`,
-  // //     {
-  // //       method: "PUT",
-  // //       headers: {
-  // //         "Content-Type": "application/json",
-  // //       },
-  // //       body: JSON.stringify({
-  // //         areasOfConcern: `${concern}`,
-  // //       }),
-  // //     }
-  // //   );
-  // //   const newEmployee = await response.json();
-  // //   console.log(newEmployee);
-  // //   //navigation.navigate('Questionnaire Two');
-  // };
+  const saveConcerns = async (concern) => {
+    console.log("concern", concern)
+    const response = await fetch(
+      `http://${ipAndPort}/api/employee/byAuthId/${user.uid}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body:// { areasOfConcern: concern },
+        JSON.stringify({
+          areasOfConcern: `${concern}`,
+        }),
+      }
+    );
+    const newEmployee = await response.json();
+    console.log(newEmployee);
+    //navigation.navigate('Questionnaire Two');
+  };
   return (
     <FlatList
       ListHeaderComponent={() => (
         <Text style={styles.quizTitle}>
-          What are your areas of concern?
+          What are you looking for help with?
           {"\n"}
           Click for more information
         </Text>
       )}
       data={CONCERNS}
       keyExtractor={(item) => item.id}
-      renderItem={renderConcernItem}
+      renderItem={(item) => renderConcernItem(item, picked, setPicked)}
       numColumns={2}
       style={{ backgroundColor: "#9AC6DF" }}
       ListFooterComponent={() => (
         <View style={{ padding: 16 }}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={pressHandler}
-          >
-          <Text style={styles.text}>{buttonText}</Text> 
-          </TouchableOpacity >
+          <TouchableOpacity style={styles.button} onPress={pressHandler}>
+            <Text style={styles.text}>{buttonText}</Text>
+          </TouchableOpacity>
         </View>
       )}
-
     />
   );
 };
@@ -126,9 +136,6 @@ const styles = StyleSheet.create({
     color: "black",
     textAlign: "center",
     marginTop: 15,
-    
-    
   },
-
 });
 export default QuestionnaireTwo;
