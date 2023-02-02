21914b00bb4661a1d7f49698f8b06b4b8f33fcae
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-02 15:50:19 -0700

    feat : data for questionnaire 3

---
+++ data/impactMentalHealth.js
@@ -0,0 +1,20 @@
+import Impact from '../models/Impact.js';
+
+
+export const IMPACT = [
+  new Impact('c1', 'Physical illness/Chronic pain'),
+  new Impact('c2', 'Parenting challenges'),
+  new Impact('c3', 'Relationships'),
+  new Impact('c4', 'Work stress'),
+  new Impact('c5', 'Burn out'),
+  new Impact('c6', 'Systemic discrimination'),
+  new Impact('c7', 'Fertility/Pregnancy/Birth'),
+  new Impact('c8', 'Gastro-intestinal/Gut issues'),
+  new Impact('c9', 'Traumatic experience'),
+  new Impact('c10', 'Weight/Body/Image concern'),
+  new Impact('c11', 'Attention/focus'),
+  new Impact('c12', 'Overwhelmed'),
+  new Impact('c13', 'Peri-menopause/Menopause'),
+  new Impact('c14', 'Grief/Loss'),
+  
+];
