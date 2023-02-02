3c19bde9b277f97f03f1a1133d9c273493af05ff
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-02 13:26:41 -0700

    feat : created a config file for ip addresses

---
+++ config.js
@@ -0,0 +1,5 @@
+export const ipAndPort = "10.44.22.75:5001"
+// export const ipAddressAndPort = "10.44.22.75:5001"
+// export const ipAddressAndPort = "10.44.22.75:5001"
+// export const ipAddressAndPort = "10.44.22.75:5001"
+// export const ipAddressAndPort = "10.44.22.75:5001"
