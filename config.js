33c737e9ec88ac449e6aafb77f0f2f1ddb82bedf
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-02-03 16:40:45 -0700

    IpConfig modified

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const ipAndPort = "10.44.22.75:5001"
+export const ipAndPort = "10.44.22.29:5001";
 // export const ipAddressAndPort = "10.44.22.75:5001"
 // export const ipAddressAndPort = "10.44.22.75:5001"
 // export const ipAddressAndPort = "10.44.22.75:5001"
