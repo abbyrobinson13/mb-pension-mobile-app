ef687670ec7132c8b66ac5d45273dd633a369225
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-02-08 10:37:11 -0700

    swiched to new IP address

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// export const ipAndPort = "10.44.22.29:5001";
-export const ipAndPort = "10.0.0.139:5001";
+export const ipAndPort = "10.44.22.29:5001";
+// export const ipAndPort = "10.0.0.139:5001";
 // export const ipAddressAndPort = "10.44.22.75:5001"
 // export const ipAddressAndPort = "10.44.22.75:5001"
 // export const ipAddressAndPort = "10.44.22.75:5001"
