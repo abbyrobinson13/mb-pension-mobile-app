039d6dac2483e95d7ba380e2db2aad0e8f7a8523
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-08 14:29:22 -0700

    removed tap groups (not needed)

--- screens/TopNavBar.js
+++ screens/TopNavBar.js
@@ -19,59 +19,51 @@ const TopNavBar = () => {
         tabBarActiveTintColor: "black",
       }}
     >
-      <TopTab.Group>
-        <TopTab.Screen
-          name="0"
-          component={Services}
-          options={{
-            tabBarLabel: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="room-service"
-                color={color}
-                size={30}
-              />
-            ),
-          }}
-        />
+      <TopTab.Screen
+        name="0"
+        component={Services}
+        options={{
+          tabBarLabel: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="room-service"
+              color={color}
+              size={30}
+            />
+          ),
+        }}
+      />
 
-        <TopTab.Screen
-          name="Providers"
-          component={Providers}
-          options={{
-            tabBarLabel: ({ color, size }) => (
-              <MaterialCommunityIcons name="doctor" color={color} size={30} />
-            ),
-          }}
-        />
-        <TopTab.Screen
-          name="Packages"
-          component={Packages}
-          options={{
-            tabBarLabel: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="package-variant"
-                color={color}
-                size={30}
-              />
-            ),
-          }}
-        />
-      </TopTab.Group>
-      <TopTab.Group>
-        <TopTab.Screen
-          name="Self-Help"
-          component={SelfHelp}
-          options={{
-            tabBarLabel: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="book-open"
-                color={color}
-                size={30}
-              />
-            ),
-          }}
-        />
-      </TopTab.Group>
+      <TopTab.Screen
+        name="Providers"
+        component={Providers}
+        options={{
+          tabBarLabel: ({ color, size }) => (
+            <MaterialCommunityIcons name="doctor" color={color} size={30} />
+          ),
+        }}
+      />
+      <TopTab.Screen
+        name="Self-Help"
+        component={SelfHelp}
+        options={{
+          tabBarLabel: ({ color, size }) => (
+            <MaterialCommunityIcons name="book-open" color={color} size={30} />
+          ),
+        }}
+      />
+      <TopTab.Screen
+        name="Packages"
+        component={Packages}
+        options={{
+          tabBarLabel: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="package-variant"
+              color={color}
+              size={30}
+            />
+          ),
+        }}
+      />
     </TopTab.Navigator>
   );
 };
