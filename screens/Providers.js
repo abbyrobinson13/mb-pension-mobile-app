6c83d1dd8a9eda163e49e3cefbfe16b8ee5d9f78
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-08 14:28:52 -0700

    added content to provider page

--- screens/Providers.js
+++ screens/Providers.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ContentList from "../components/ContentList";
 
 const Providers = () => {
   return (
     <View style={styles.container}>
-      <Text>Providers</Text>
+      <ContentList category="Practitioners" />
     </View>
   );
 };
