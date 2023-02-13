995eb860f2387a4a001872c8e4faaf9c0d7643ca
Afshin Sharifnia <afshin.sharifnia@gmail.com>
2023-02-12 19:47:02 -0700

    alert added to logout

--- screens/LoggingOut.js
+++ screens/LoggingOut.js
@@ -1,14 +1,19 @@
-import {getAuth, signOut} from 'firebase/auth';
-import {Button} from 'react-native';
+import { getAuth, signOut } from "firebase/auth";
+import { Button, Alert } from "react-native";
 
-const auth = getAuth ();
+const auth = getAuth();
 
-export default function LoggingOut({navigation}) {
+export default function LoggingOut({ navigation }) {
   let logout = () => {
-    signOut (auth).then (() => {
-      navigation.popToTop ();
+    signOut(auth).then(() => {
+      Alert.alert("Success", "logged out successfully!", [
+        {
+          text: "OK",
+          onPress: () => navigation.popToTop("MB Pension and Benefits Group"),
+        },
+      ]);
     });
   };
 
-  return <Button title="Logout" onPress={logout} color={'#01796f'} />;
+  return <Button title="Logout" onPress={logout} color={"#01796f"} />;
 }
