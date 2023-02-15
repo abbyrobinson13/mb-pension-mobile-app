c97b7e46ef7be884f6f15e846c1d670a764a2224
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-15 13:37:56 -0700

    added banner to providers

--- components/ContentList.js
+++ components/ContentList.js
@@ -1,28 +1,63 @@
 import { ScrollView, StyleSheet, Linking, View } from "react-native";
-import React, { useState, useEffect } from "react";
-import { Card, Text, Button, Avatar } from "react-native-paper";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  Card,
+  Text,
+  Button,
+  ActivityIndicator,
+  Drawer,
+} from "react-native-paper";
 import { ipAndPort } from "../config";
+import { AuthContext } from "../AuthProvider.js";
 
 const ContentList = ({ category }) => {
   const [content, setContent] = useState(null);
-  console.log(ipAndPort);
+  const [benefits, setBenefits] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [employees, setEmployees] = useState(null);
+
+  const authContext = useContext(AuthContext);
+  const user = authContext.user;
+  console.log(user.uid);
 
   useEffect(() => {
-    const getContent = async () => {
+    const fetchData = async () => {
       try {
-        let response = await fetch(`http://${ipAndPort}/api/content`);
-        let data = await response.json();
-        setContent(data);
+        const [contentResponse, benefitsResponse, employeesResponse] =
+          await Promise.all([
+            fetch(`http://${ipAndPort}/api/content`),
+            fetch(`http://${ipAndPort}/api/benefits`),
+            fetch(`http://${ipAndPort}/api/employee`),
+          ]);
+        const [contentData, benefitsData, employeesData] = await Promise.all([
+          contentResponse.json(),
+          benefitsResponse.json(),
+          employeesResponse.json(),
+        ]);
+        setContent(contentData);
+        setBenefits(benefitsData);
+        setEmployees(employeesData);
+        setIsLoading(false);
       } catch (ex) {
         console.error(`Problems fetching: ${ex.message}`);
       }
     };
-    getContent();
+    fetchData();
   }, []);
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
-  const handlePress = (url) => {
-    Linking.openURL(row.url);
-  };
+  const userProfile = employees.find((employee) => employee.uid === user.uid);
+  const userBenefits = benefits.find(
+    (plan) => plan.companyName === userProfile.companyName
+  );
+  const paramedicalList = userBenefits?.insuranceCompany.value;
+  console.log(paramedicalList);
 
   return (
     <ScrollView style={styles.container}>
@@ -32,6 +67,22 @@ const ContentList = ({ category }) => {
           .map((row) => (
             <Card style={styles.card}>
               <Card.Content>
+                {category === "Practitioners" &&
+                  paramedicalList.filter((item) => item === row.provider)
+                    .length > 0 && (
+                    <Drawer.Item
+                      style={{
+                        backgroundColor: "#e8e68a",
+                        height: 30,
+                        marginLeft: 0,
+                        width: 300,
+                        marinTop: 20,
+                        marginBottom: 20,
+                      }}
+                      icon="star"
+                      label={row.provider + " You're covered"}
+                    />
+                  )}
                 <Text variant="titleLarge">{row.title}</Text>
                 <Text variant="bodyMedium">{row.description}</Text>
               </Card.Content>
@@ -54,7 +105,7 @@ export default ContentList;
 
 const styles = StyleSheet.create({
   card: {
-    height: 350,
+    height: 400,
     width: 325,
     margin: 20,
   },
