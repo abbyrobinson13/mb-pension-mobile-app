6c43554ecc496171ddfe9b705e9ad59b454a7722
Abigail Robinson <abbyrobinson@shaw.ca>
2023-02-17 08:39:52 -0700

    ip update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// export const ipAndPort = "10.44.22.29:5001";
+export const ipAndPort = "10.44.22.24:5001";
 // export const ipAndPort = "10.0.0.139:5001";
 // export const ipAndPort = "192.168.68.51:5001";
 //export const ipAndPort = "192.168.1.68:5001"
 // export const ipAndPort = "10.44.22.75:5001"
 // export const ipAndPort = "10.44.22.75:5001"
-export const ipAndPort = "192.168.68.51:5001";
+// export const ipAndPort = "192.168.68.51:5001";
