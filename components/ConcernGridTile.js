1d75456436dccd1c794bc74e3dbcf00b4ac25a86
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-18 04:16:19 -0700

    style: padding for modal

--- components/ConcernGridTile.js
+++ components/ConcernGridTile.js
@@ -16,9 +16,13 @@ const ConcernGridTile = ({ title, picked, setPicked, image }) => {
   const [isModalVisible, setModalVisible] = useState(false);
   const [selected, setSelected] = useState(false);
   const modalIcon = require("../assets/information.png");
+  const [iconTapped, setIconTapped] = useState(false);
   //const modalImage = require("../assets/TRAUMA.png");
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
+    if (isModalVisible) {
+      setIconTapped(false);
+    }
   };
   return (
     <View
@@ -41,9 +45,17 @@ const ConcernGridTile = ({ title, picked, setPicked, image }) => {
         }}
       >
         <View style={styles.innerContainer}>
-          <TouchableOpacity onPress={toggleModal}>
+          <TouchableOpacity
+            onPress={() => {
+              toggleModal();
+              setIconTapped(!iconTapped);
+            }}
+          >
             <Image
-              style={styles.infoButton}
+              style={[
+                styles.infoButton,
+                { tintColor: iconTapped ? "#e1705d" : "#0f1a4d" },
+              ]}
               source={modalIcon}
               size={30}
               color="black"
@@ -55,11 +67,11 @@ const ConcernGridTile = ({ title, picked, setPicked, image }) => {
             >
               <View style={styles.modalInnerContainer}>
                 <Image source={image} size={30} />
-                <Button title="Close" onPress={toggleModal} />
+                <Button title="Close" onPress={toggleModal} color="#e1705d" style={{ borderRadius: 200, width: 50, marginTop: 20}}/>
               </View>
             </Modal>
           </TouchableOpacity>
-          <Text style={styles.title}>{title}</Text>
+          <Text style={[styles.title, { color: "#0f1a4d" }]}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -72,8 +84,8 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 10,
     height: 100,
-    width:50,
-    borderRadius: 6,
+    width: 50,
+    borderRadius: 30,
     elevation: 4,
     backgroundColor: "white",
     shadowColor: "black",
@@ -81,14 +93,24 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     overflow: Platform.OS === "hidden" ? "hidden" : "visible",
-
   },
   button: {
-    flex: 1
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  //   borderRadius: 6,
+  //   borderWidth: 1,
+  // borderColor: "#0f1a4d"
   },
   buttonPressed: {
-    //opacity: 0.5,
-    backgroundColor: "orange",
+  
+    backgroundColor: "#FAF5F3",
+    borderColor:"#e1705d",
+    borderWidth: 1,
+    
+    
+   
+    
   },
   innerContainer: {
     flex: 1,
@@ -99,10 +121,8 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "bold",
     fontSize: 16,
-    textAlign:"center",
+    textAlign: "center",
     //marginRight: 10,
-    
-   
   },
   modalContainer: {
     flex: 1,
@@ -112,18 +132,25 @@ const styles = StyleSheet.create({
   },
   modalInnerContainer: {
     backgroundColor: "white",
+    borderRadius: 10,
+    
+  // padding: 5,
+  // alignItems: "center",
+  // justifyContent: "center",
+  
   },
 
   infoButton: {
-     position:"relative",
+    position: "relative",
     top: 0,
     left: 0,
-    margin : 10,
+    margin: 10,
   },
 
   modal: {
     justifyContent: "center",
     flexDirection: "column",
     alignItems: "center",
+    
   },
 });
