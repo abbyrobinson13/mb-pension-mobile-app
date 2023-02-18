f462ee6ee2c93c87e1a005d9901ec1f1e8ba438d
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-18 05:36:32 -0700

    adjusted class names

--- data/areasOfConcern.js
+++ data/areasOfConcern.js
@@ -2,14 +2,14 @@ import Concern from '../models/Concern.js';
 
 
 export const CONCERNS = [
-  new Concern('c1', 'ADHD',require ('../assets/adhd.png')),
+  new Concern('c1', 'Attention Deficit Hyperactivity Disorder (ADHD)',require ('../assets/adhd.png')),
   new Concern('c2', 'Addiction',require ('../assets/addiction.png')),
   new Concern('c3', 'Anxiety',require ('../assets/anxiety.png')),
   new Concern('c4', 'Insomnia',require ('../assets/modal.png')),
   new Concern('c5', 'Depression',require ('../assets/modal.png')),
   new Concern('c6', 'Trauma',require ('../assets/TRAUMA.png')),
   new Concern('c7', 'Eating Disorders',require ('../assets/modal.png')),
-  new Concern('c8', 'Emotional Regulation Problem',require ('../assets/modal.png')),
+  new Concern('c8', 'Emotional Regulation Problems',require ('../assets/modal.png')),
   
 ];
 
