1f87b9a4ab4910b7d19c9415e6defb6e9a0b8a7a
Tosin-Kasaba <tosinkasaba1@gmail.com>
2023-02-18 03:58:31 -0700

    feat: modal content

--- data/areasOfConcern.js
+++ data/areasOfConcern.js
@@ -2,9 +2,9 @@ import Concern from '../models/Concern.js';
 
 
 export const CONCERNS = [
-  new Concern('c1', 'ADHD',require ('../assets/modal.png')),
-  new Concern('c2', 'Addiction',require ('../assets/modal.png')),
-  new Concern('c3', 'Anxiety',require ('../assets/modal.png')),
+  new Concern('c1', 'ADHD',require ('../assets/adhdd.png')),
+  new Concern('c2', 'Addiction',require ('../assets/addiction.png')),
+  new Concern('c3', 'Anxiety',require ('../assets/anxiety.png')),
   new Concern('c4', 'Insomnia',require ('../assets/modal.png')),
   new Concern('c5', 'Depression',require ('../assets/modal.png')),
   new Concern('c6', 'Trauma',require ('../assets/TRAUMA.png')),
